686270cad21d2de52a49d8aa8343c6784559d836
vaithiyanathan <vasanthvaithy@gmail.com>
2025-04-07 18:45:47 +0530

    Solved 2 program using BrutForce 1. Longest Palindromic Substring 2. Rverse vowels of given string

--- Java/src/Strings/LongestPalidromicSubstring.java
+++ Java/src/Strings/LongestPalidromicSubstring.java
@@ -5,27 +5,49 @@ import java.util.List;
 
 public class LongestPalidromicSubstring {
 	
-	public static void main(String args[]) {
-		String input = "bacad";
-		int length = input.length();
-		String subarray = new String();
-
-		 int  count =1;
-		 for(int j=0; j< length;j++) {
-		for(int i =j;i<length;i++) {
-			subarray = subarray+input.charAt(i);
-			if(subarray.contains(input.substring(i, i)) && subarray.length()>1 ) {
-				System.out.println(subarray);
-				//System.out.println(subarray.toString());
-				String reversedsubarray = new StringBuilder(subarray).reverse().toString();
-				if(reversedsubarray.equals(subarray)) {
-					System.out.println(reversedsubarray);
-					System.out.println(subarray);
-				}
+	public static boolean isPalindrome(String s) {
+		
+	 char[] charr = s.toCharArray();
+	 int length = charr.length-1;
+	 for(int i =0; i<charr.length-1;i++) {
+		 	
+			 if(charr[i]!=charr[length])
+			 {
+				 
+				 return false;
+			 }
+			 length--;
+			 
+		 		 
+	 }
+		return true;
+	}
+	public static String longestPalindromicSubString(String s) {
+		
+		 int max =0;
+		 String subStr ="";
+         if(s.length()<=2){return s.substring(0, 1);}
+         else{
+		for(int i =0; i<s.length(); i++) {
+			for( int j =i+1; j<s.length(); j++) {
+			if(isPalindrome(s.substring(i, j+1))) {
 				
+				if(max<j-i+1) {
+					max =j-i+1;
+					subStr = s.substring(i, j+1);
+				}
 			}
-		}
-		 }
+			}
+		}	
+         }
+		return subStr;  
+    }
+	public static void main(String args[]) {
+		String input = "ac";
+//		int length = input.length();
+//		String subarray = new String();
+      System.out.println(longestPalindromicSubString(input));
+		
 	}
 
 }
